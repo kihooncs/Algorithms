9825dc546d90acad2535829d1dc28514cc3109d0
Kihoon Lee <kihooncs@gmail.com>
2022-01-24 00:19:06 -0800

    Jan 24th Commit Challenge

--- Patterns/Merge_Intervals/MergeIntervals.java
+++ Patterns/Merge_Intervals/MergeIntervals.java
@@ -10,10 +10,59 @@ class MergeIntervals {
             this.end = end;
         }
     };
-
+    /*
+     *
+     *
+     */
     public static List<Interval> mergeBF(List<Interval> intervals) {
+        // At most O(n) space at worst case.
         List<Interval> mergedIntervals = new LinkedList<Interval>();
-        // TODO: Write your code here
+        // O(nlogn) for sorting
+        // Java's collections.sort() either uses Merge sort or Timsort, and both these algorithms
+        // need O(N) space. Overall, our algorithm has a space complexity of O(N)
+        Collections.sort(intervals, (a, b) -> Integer.compare(a.start, b.start));
+        Interval a = intervals.get(0);
+        Interval c = new Interval(a.start, a.end);
+        // O(n) for traversing
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval b = intervals.get(i);
+            if (b.start <= c.end) {
+                c.end = Math.max(b.end, c.end);
+            } else {
+                mergedIntervals.add(c);
+                c = new Interval(b.start, b.end);
+            }
+        }
+        mergedIntervals.add(c);
+        return mergedIntervals;
+    }
+
+    public static List<Interval> merge(List<Interval> intervals) {
+        if (intervals.size() < 2)
+            return intervals;
+
+        // sort the intervals by start time
+        Collections.sort(intervals, (a, b) -> Integer.compare(a.start, b.start));
+
+        List<Interval> mergedIntervals = new LinkedList<Interval>();
+        Iterator<Interval> intervalItr = intervals.iterator();
+        Interval interval = intervalItr.next();
+        int start = interval.start;
+        int end = interval.end;
+
+        while (intervalItr.hasNext()) {
+            interval = intervalItr.next();
+            if (interval.start <= end) { // overlapping intervals, adjust the 'end'
+                end = Math.max(interval.end, end);
+            } else { // non-overlapping interval, add the previous interval and reset
+                mergedIntervals.add(new Interval(start, end));
+                start = interval.start;
+                end = interval.end;
+            }
+        }
+        // add the last interval
+        mergedIntervals.add(new Interval(start, end));
+
         return mergedIntervals;
     }
 
