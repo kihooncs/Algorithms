88034e61d12d6c0573410dc4a80a717ae21a094c
Kihoon Lee <kihooncs@gmail.com>
2022-01-29 16:17:45 -0800

    Jan 29th commit challenge

--- Patterns/Tree_Breadth_First_Search/LevelOrderTraversal.java
+++ Patterns/Tree_Breadth_First_Search/LevelOrderTraversal.java
@@ -1,5 +1,7 @@
 package Patterns.Tree_Breadth_First_Search;
 
+import com.sun.source.tree.Tree;
+
 import java.util.*;
 
 /*
@@ -9,18 +11,40 @@ import java.util.*;
 public class LevelOrderTraversal {
     public static List<List<Integer>> traverseBF(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        // TODO: Write your code here
+        Queue<TreeNode> nodes = new LinkedList<>();
+        nodes.add(root);
+
         return result;
     }
 
     public static List<List<Integer>> traverse(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        // TODO: Write your code here
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                // add the node to the current level
+                currentLevel.add(currentNode.val);
+                // insert the children of current node in the queue
+                if (currentNode.left != null)
+                    queue.offer(currentNode.left);
+                if (currentNode.right != null)
+                    queue.offer(currentNode.right);
+            }
+            result.add(currentLevel);
+        }
+
         return result;
     }
 
     public static void main(String[] args) {
-        bruteForce();
+        //bruteForce();
         solution();
     }
 
