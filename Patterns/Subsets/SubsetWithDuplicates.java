b7a4d4459253b0e5d119686b5d02eb3384289070
Kihoon Lee <kihooncs@gmail.com>
2022-02-02 23:36:19 -0800

    Feb 2nd Commit Challenge: Working on Subsets With Duplicates

---
+++ Patterns/Subsets/SubsetWithDuplicates.java
@@ -0,0 +1,19 @@
+package Patterns.Subsets;
+
+import java.util.*;
+
+public class SubsetWithDuplicates {
+    public static List<List<Integer>> findSubsets(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        // TODO: Write your code here
+        return subsets;
+    }
+
+    public static void main(String[] args) {
+        List<List<IntSubsetWithDuplicateseger>> result = SubsetWithDuplicates.findSubsets(new int[] { 1, 3, 3 });
+        System.out.println("Here is the list of subsets: " + result);
+
+        result = SubsetWithDuplicates.findSubsets(new int[] { 1, 5, 3, 3 });
+        System.out.println("Here is the list of subsets: " + result);
+    }
+}
